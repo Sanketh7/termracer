7f035e7dcc48f54124ae26f57ab68bbce0e05028
Sanketh Varamballi <sankethv03@gmail.com>
2023-01-01 00:59:14 -0800

    clean

--- src/constants.rs
+++
@@ -1,5 +0,0 @@
-// used for calculating WPM
-pub const CHARACTERS_PER_WORD: usize = 5;
-
-// stores english words used by WordGenerator
-pub const WORDS_STRING: &str = include_str!("../assets/english_words.txt");
--- src/widgets/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod line_block_widget;
-pub mod line_widget;
-pub mod session_widget;
-pub mod stats_line_widget;
-pub mod widget;
