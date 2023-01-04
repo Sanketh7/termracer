07a84473262a69bee8059d5d7baf61cdf9dce88a
Sanketh Varamballi <sankethv03@gmail.com>
2023-01-04 14:50:58 -0800

    created basic game ui

--- termracer_client/src/main.rs
+++ termracer_client/src/main.rs
@@ -1,20 +1,11 @@
-use crossterm::{
-    event::{self, Event, KeyCode},
-    execute, terminal,
-};
-use layout::HorizontalSplitKind;
-use std::io::{self, Write};
+use crossterm::{execute, terminal};
+use solo_game::SoloGame;
+use std::io;
 use std::time::Duration;
-use unicode_segmentation::UnicodeSegmentation;
-use views::{
-    line_block::LineBlock,
-    stats_line::StatsLine,
-    view::{KeyEventHandleable, View},
-};
-use window::Window;
 
 mod layout;
 mod rect;
+mod solo_game;
 mod views;
 mod window;
 
@@ -24,44 +15,8 @@ fn main() {
         .expect("ERROR: Failed to enter alternate screen.");
     terminal::enable_raw_mode().expect("ERROR: Failed to enable raw mode.");
 
-    let (term_width, term_height) = terminal::size().expect("ERROR: Failed to get terminal size.");
-
-    let mut window = Window::new(term_width, term_height);
-    let (block_region, stats_region) =
-        window.horizontal_split(HorizontalSplitKind::CellsInBottom(1), 0);
-
-    let text = "A lot of sample text oh boy\n".repeat(49).to_owned();
-    let text_lines = text
-        .split('\n')
-        .map(|line| line.graphemes(true).map(String::from).collect())
-        .collect();
-    let mut block = LineBlock::new(text_lines, block_region);
-    let mut stats = StatsLine::new(stats_region);
-    let mut wpm = 0.0;
-
-    loop {
-        if event::poll(Duration::from_millis(10)).expect("ERROR: Failed to poll event.") {
-            match event::read().expect("ERROR: Failed to read event.") {
-                Event::Key(key_event) => match key_event.code {
-                    KeyCode::Esc => break,
-                    _ => block.handle_key_event(key_event),
-                },
-                Event::Resize(width, height) => {
-                    window.resize(width, height);
-                    window.clear();
-                }
-                _ => (),
-            }
-        } else {
-            stats.set_wpm(wpm);
-            block.draw(&mut window);
-            stats.draw(&mut window);
-            window.display(&mut buf);
-            block.reset_cursor(&mut window);
-            buf.flush().expect("ERROR: Failed to flush buffer.");
-            wpm += 10.0;
-        }
-    }
+    let mut game = SoloGame::new();
+    game.game_loop(&mut buf, Duration::from_millis(10));
 
     terminal::disable_raw_mode().expect("ERROR: Failed to disable raw mode.");
     execute!(buf, terminal::LeaveAlternateScreen)
---
+++ termracer_client/src/solo_game.rs
@@ -0,0 +1,88 @@
+use std::{
+    io::{self, Write},
+    time::Duration,
+};
+
+use crate::{
+    layout::HorizontalSplitKind,
+    views::{
+        line_block::LineBlock,
+        stats_line::StatsLine,
+        view::{KeyEventHandleable, View},
+    },
+    window::Window,
+};
+use crossterm::{
+    event::{self, Event, KeyCode},
+    terminal,
+};
+use unicode_segmentation::UnicodeSegmentation;
+
+struct UI {
+    window: Window,
+    // views
+    line_block: LineBlock,
+    stats_line: StatsLine,
+}
+
+pub struct SoloGame {
+    ui: UI,
+}
+
+impl SoloGame {
+    pub fn new() -> Self {
+        let text = "A lot of sample text oh boy\n".repeat(49).to_owned();
+        let text_lines = text
+            .split('\n')
+            .map(|line| line.graphemes(true).map(String::from).collect())
+            .collect();
+        let (term_width, term_height) =
+            terminal::size().expect("ERROR: Failed to get terminal size.");
+        let mut window = Window::new(term_width, term_height);
+        let (line_block_region, stats_line_region) =
+            window.horizontal_split(HorizontalSplitKind::CellsInBottom(1), 0);
+        let line_block = LineBlock::new(text_lines, line_block_region);
+        let stats_line = StatsLine::new(stats_line_region);
+
+        SoloGame {
+            ui: UI {
+                window,
+                line_block,
+                stats_line,
+            },
+        }
+    }
+
+    pub fn game_loop<T: Write>(&mut self, buf: &mut T, poll_duration: Duration) {
+        let mut wpm = 0.0;
+
+        loop {
+            if event::poll(poll_duration).expect("ERROR: Failed to poll event.") {
+                match event::read().expect("ERROR: Failed to read event.") {
+                    Event::Key(key_event) => match key_event.code {
+                        KeyCode::Esc => break,
+                        _ => self.ui.line_block.handle_key_event(key_event),
+                    },
+                    Event::Resize(width, height) => {
+                        self.ui.window.resize(width, height);
+                        self.ui.window.clear();
+                    }
+                    _ => (),
+                }
+            } else {
+                // update state
+                wpm += 10.0;
+                self.ui.stats_line.set_wpm(wpm);
+
+                // draw to window
+                self.ui.line_block.draw(&mut self.ui.window);
+                self.ui.stats_line.draw(&mut self.ui.window);
+                self.ui.line_block.reset_cursor(&mut self.ui.window);
+
+                // display window on screen
+                self.ui.window.display(buf);
+                buf.flush().expect("ERROR: Failed to flush buffer.");
+            }
+        }
+    }
+}
