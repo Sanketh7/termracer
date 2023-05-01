14ca8dc0d875fcc0cc3691ff0055b605af9267f0
Sanketh Varamballi <sankethv03@gmail.com>
2023-05-01 16:52:52 -0700

    Add CLI support

--- termracer_client/src/main.rs
+++ termracer_client/src/main.rs
@@ -1,15 +1,12 @@
-#[macro_use]
-extern crate lazy_static;
-
-use std::io::{self, Write};
+use std::io;
 use std::time::Duration;
 
+use clap::Parser;
 use crossterm::{execute, style};
+use util::cli::{Cli, Commands};
 
 use crate::game::solo_game::SoloGame;
 use crate::models::game_result::GameResult;
-use crate::util::command;
-use crate::util::command::Command;
 
 mod framework;
 mod game;
@@ -18,32 +15,20 @@ mod util;
 mod views;
 
 fn main() {
-  loop {
-    print!("> ");
-    io::stdout().flush().unwrap();
+  let cli = Cli::parse();
 
-    let mut line = String::new();
-    io::stdin()
-      .read_line(&mut line)
-      .expect("ERROR: Failed to read line.");
-    line = line.trim_end().to_string();
+  match cli.command {
+    Commands::Solo { word_count } => {
+      let mut buf = io::stdout().lock();
 
-    match command::parse(&line) {
-      Some(Command::Start(word_count)) => {
-        let mut buf = io::stdout().lock();
+      let mut game = SoloGame::new(word_count);
+      let game_results = game.run(&mut buf, Duration::from_millis(1000 / 30));
 
-        let mut game = SoloGame::new(word_count);
-        let game_results = game.run(&mut buf, Duration::from_millis(1000 / 30));
-
-        let end_text = match game_results {
-          GameResult::Completed { wpm } => format!("WPM: {}\n", wpm as u32),
-          GameResult::Aborted => "Aborted!\n".to_string(),
-        };
-        execute!(buf, style::Print(end_text)).expect("ERROR: Failed to print end text.");
-      }
-      Some(Command::Help) => println!("{}", *command::HELP_TEXT),
-      Some(Command::Quit) => break,
-      None => println!("No such command.\nUse `help` to see a list of commands."),
+      let end_text = match game_results {
+        GameResult::Completed { wpm } => format!("WPM: {}\n", wpm as u32),
+        GameResult::Aborted => "Aborted!\n".to_string(),
+      };
+      execute!(buf, style::Print(end_text)).expect("ERROR: Failed to print end text.");
     }
   }
 }
